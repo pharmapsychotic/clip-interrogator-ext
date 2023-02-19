31a3a440d3a6e72266706dde04e98907f5845613
pharmapsychotic <me@pharmapsychotic.com>
2023-02-18 22:31:59 -0600

    Update clip-interrogator version

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
 import launch
 
 if not launch.is_installed("clip-interrogator"):
-    launch.run_pip("install clip-interrogator==0.5.0", "requirements for CLIP Interrogator")
+    launch.run_pip("install clip-interrogator==0.5.1", "requirements for CLIP Interrogator")
